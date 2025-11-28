c58350fd04deb359466b963c5881dd939a96e723
Holy-Farus <davvidkassuman@gmail.com>
2025-11-28 12:48:52 +0300

    Model

---
+++ Model.js
@@ -0,0 +1,21 @@
+const fsp = require('fs/promises');
+const { EOL } = require('os');
+
+class Model {
+  constructor() {
+    this.questions = [];
+  }
+
+  async readFile() {
+    const content = await fsp.readFile('./topics/questions.txt', 'utf-8');
+    const dataObj = (data) =>
+      data.split(EOL + EOL).map((el) => {
+        const [q, a] = el.split(EOL);
+        return { q, a };
+      });
+    this.questions.push(...dataObj(content));
+  }
+}
+
+
+module.exports = Model
